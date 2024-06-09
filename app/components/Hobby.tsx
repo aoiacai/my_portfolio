4a4220292e58af45a6ccb6137fe8809d7626eaf9
aoiacai <102672343+aoiacai@users.noreply.github.com>
2024-06-09 14:49:55 +0900

    Update Hobby.tsx

--- app/components/Hobby.tsx
+++ app/components/Hobby.tsx
@@ -6,33 +6,38 @@ import { Avatar, Link, Typography } from '@mui/material';
 const Hobby: React.FC = () => {
     return (
         <div className='profile' id='hobby' style={{ paddingTop: '20px' }}>
-            <Typography style={{ fontSize: '7vh', padding: '5vh' }}>Hobby</Typography>
+            <Typography style={{ fontSize: '12vmin', padding: '4vmin' }}>Hobby</Typography>
             <div className='text-area'>
                 <div className='text_and_icon'>
                     <div style={{ display: 'block' }}>
-                        <Typography style={{ fontSize: '2vh', color: 'cyan' }}>GAME</Typography>
+                        <div className='centerClass'>
+                            <Typography style={{ fontSize: '3vmin', color: 'cyan' }}>GAME</Typography>
+                        </div>
                         <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
-                            <Typography style={{ fontSize: '3vh' }}>stardew valley</Typography>
-                            <Typography style={{ fontSize: '3vh' }}>AtCoder</Typography>
+                            <Typography style={{ fontSize: '4vmin' }}>stardew valley</Typography>
+                            <Typography style={{ fontSize: '4vmin' }}>AtCoder</Typography>
                         </div>
 
-                        <Typography style={{ fontSize: '2vh', color: 'cyan' }}>COMIC</Typography>
-                        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
-                            <Typography style={{ fontSize: '3vh', paddingLeft: '1vh', paddingRight: '1vh' }}>結界師</Typography>
-                            <Typography style={{ fontSize: '3vh', paddingLeft: '1vh', paddingRight: '1vh' }}>サマータイムレンダ</Typography>
-                            <Typography style={{ fontSize: '3vh', paddingLeft: '1vh', paddingRight: '1vh' }}>チ。</Typography>
-                            <Typography style={{ fontSize: '3vh', paddingLeft: '1vh', paddingRight: '1vh' }}>呪術廻戦</Typography>
-                            <Typography style={{ fontSize: '3vh', paddingLeft: '1vh', paddingRight: '1vh' }}>エヴァンゲリオン</Typography>
-                            <Typography style={{ fontSize: '3vh', paddingLeft: '1vh', paddingRight: '1vh' }}>不滅のあなたへ</Typography>
-                            <Typography style={{ fontSize: '3vh', paddingLeft: '1vh', paddingRight: '1vh' }}>ダンジョン飯</Typography>
-                            <Typography style={{ fontSize: '3vh', paddingLeft: '1vh', paddingRight: '1vh' }}>etc.</Typography>
+                        <div className='centerClass'>
+                            <Typography style={{ fontSize: '3vmin', color: 'cyan' }}>COMIC</Typography>
+                        </div>
+                        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly', gap: '3vmin' }}>
+                            <div style={{ fontSize: '4vmin' }}>結界師</div>
+                            <div style={{ fontSize: '4vmin' }}>サマータイムレンダ</div>
+                            <div style={{ fontSize: '4vmin' }}>チ。</div>
+                            <div style={{ fontSize: '4vmin' }}>呪術廻戦</div>
+                            <div style={{ fontSize: '4vmin' }}>エヴァンゲリオン</div>
+                            <div style={{ fontSize: '4vmin' }}>不滅のあなたへ</div>
+                            <div style={{ fontSize: '4vmin' }}>ダンジョン飯</div>
+                            <div style={{ fontSize: '4vmin' }}>etc.</div>
                         </div>
 
 
-                        <Typography style={{ fontSize: '2vh', color: 'cyan' }}>Music</Typography>
+                        <div className='centerClass'>
+                            <Typography style={{ fontSize: '3vmin', color: 'cyan' }}>Music</Typography>
+                        </div>
                         <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
-
-                            <iframe style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/track/6UHwmfYfRm445uyyM9HO3o?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                            <iframe style={{ borderRadius: '12px', maxWidth: "90%", height: '30vh' }} src="https://open.spotify.com/embed/track/6UHwmfYfRm445uyyM9HO3o?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         </div>
                     </div>
                 </div>
