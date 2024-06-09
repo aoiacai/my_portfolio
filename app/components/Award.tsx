e6352707e393e44ec2573af23c824630225876e8
aoiacai <102672343+aoiacai@users.noreply.github.com>
2024-06-09 11:49:16 +0900

    Update Award.tsx

--- app/components/Award.tsx
+++ app/components/Award.tsx
@@ -11,14 +11,14 @@ const Award: React.FC = () => {
                 <div className='text_and_icon'>
                     <div style={{ display: 'block' }}>
                         <Typography style={{ fontSize: '2vh', color: 'cyan' }}>2023</Typography>
-                        <Typography style={{ fontSize: '3vh' }}>高専プロコン競技部門優勝 チーム「蟹高専」アルゴリズム担当</Typography>
-                        <Typography style={{ fontSize: '3vh' }}>文部科学大臣賞</Typography>
-                        <Typography style={{ fontSize: '3vh' }}>情報処理学会若手奨励賞</Typography>
-                        <Typography style={{ fontSize: '3vh' }}>電子情報通信学会若手奨励賞</Typography>
-                        <Typography style={{ fontSize: '3vh' }}>The 15th NAPROCK INTERNATIONAL PROGRAMMING CONTEST Competition section Champion </Typography>
+                        <Typography style={{ fontSize: '3vh' }}>・高専プロコン競技部門優勝 チーム「蟹高専」アルゴリズム担当</Typography>
+                        <Typography style={{ fontSize: '3vh' }}>・文部科学大臣賞</Typography>
+                        <Typography style={{ fontSize: '3vh' }}>・情報処理学会若手奨励賞</Typography>
+                        <Typography style={{ fontSize: '3vh' }}>・電子情報通信学会若手奨励賞</Typography>
+                        <Typography style={{ fontSize: '3vh' }}>・The 15th NAPROCK INTERNATIONAL PROGRAMMING CONTEST Competition section Champion </Typography>
 
                         <Typography style={{ fontSize: '2vh', color: 'cyan' }}>2024</Typography>
-                        <Typography style={{ fontSize: '3vh' }}>AI-STEP 準奨励賞</Typography>
+                        <Typography style={{ fontSize: '3vh' }}>・AI-STEP 準奨励賞</Typography>
                     </div>
                 </div>
             </div>
