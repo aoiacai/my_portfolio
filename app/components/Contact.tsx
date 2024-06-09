b72977b458524f0922e52ebcac966d71fb32fccf
aoiacai <102672343+aoiacai@users.noreply.github.com>
2024-06-09 14:47:58 +0900

    Update Contact.tsx

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -6,12 +6,12 @@ import { Avatar, Link, Typography } from '@mui/material';
 const Contact: React.FC = () => {
     return (
         <div className='profile' id='contact' style={{ paddingTop: '20px' }}>
-            <Typography style={{ fontSize: '7vh', padding: '5vh' }}>Contact</Typography>
+            <Typography style={{ fontSize: '12vmin', padding: '4vmin' }}>Contact</Typography>
             <div className='text-area'>
                 <div className='text_and_icon'>
                     <div style={{ display: 'block' }}>
-                        <Typography style={{ fontSize: '2vh', color: 'cyan' }}>Publication scheduled.</Typography>
-                        <Typography style={{ fontSize: '3vh' }}>xxxx@example.com</Typography>
+                        <Typography style={{ fontSize: '3vmin', color: 'cyan' }}>Publication scheduled.</Typography>
+                        <Typography style={{ fontSize: '4vmin' }}>xxxx@example.com</Typography>
                     </div>
                 </div>
             </div>
