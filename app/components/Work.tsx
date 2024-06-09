d005db36d47972f22764e0e9e62ef1a498c092ce
aoiacai <102672343+aoiacai@users.noreply.github.com>
2024-06-09 11:50:46 +0900

    Update Work.tsx

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -13,13 +13,13 @@ const Work: React.FC = () => {
                     <div style={{ display: 'block' }}>
                         <Typography style={{ fontSize: '2vh' }}>高専プロコン競技部門 GUI</Typography>
                         <a href="https://github.com/aoiacai/procon34_Open">
-                            <img src="https://github.com/aoiacai/procon34_Open/blob/main/pic.png?raw=true" alt="Procon34 Open" style={{ height: '40vh' }} />
+                            <img src="https://github.com/aoiacai/procon34_Open/blob/main/pic.png?raw=true" alt="Procon34 Open" style={{ maxHeight: '40vh', maxWidth: '100%' }} />
                         </a>
                     </div>
                     <div style={{ display: 'block' }}>
                         <Typography style={{ fontSize: '2vh' }}>Pomodoro Timer</Typography>
                         <a href="https://aoiacai.github.io/pomodoro_simple">
-                            <img src="https://github.com/aoiacai/pomodoro_simple/blob/main/pic.png?raw=true" alt="Procon34 Open" style={{ height: '40vh' }} />
+                            <img src="https://github.com/aoiacai/pomodoro_simple/blob/main/pic.png?raw=true" alt="Procon34 Open" style={{ maxHeight: '40vh', maxWidth: '100%'}} />
                         </a>
                     </div>
                 </div>
