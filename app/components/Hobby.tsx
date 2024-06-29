4a2546c49a985f343d8d7a9e5b4cda32b70e5f97
tomo <09bkhair19@gmail.com>
2024-06-29 22:20:43 +0000

    spotifyを更新

--- app/components/Hobby.tsx
+++ app/components/Hobby.tsx
@@ -6,7 +6,7 @@ import { Avatar, Link, Typography } from '@mui/material';
 const Hobby: React.FC = () => {
     return (
         <div className='profile' id='hobby' style={{ paddingTop: '65px' }}>
-            <Typography style={{ fontSize: '12vmin', paddingLeft: '4vmin', paddingBottom: '4vmin'}}>Hobby</Typography>
+            <Typography style={{ fontSize: '12vmin', paddingLeft: '4vmin', paddingBottom: '4vmin' }}>Hobby</Typography>
             <div className='text-area'>
                 <div className='text_and_icon'>
                     <div style={{ display: 'block' }}>
@@ -21,7 +21,7 @@ const Hobby: React.FC = () => {
                         <div className='centerClass'>
                             <Typography style={{ fontSize: '3vmin', color: 'cyan' }}>COMIC</Typography>
                         </div>
-                        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly', gap: '3vmin' , paddingBottom: '5vh'}}>
+                        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly', gap: '3vmin', paddingBottom: '5vh' }}>
                             <div style={{ fontSize: '4vmin' }}>結界師</div>
                             <div style={{ fontSize: '4vmin' }}>サマータイムレンダ</div>
                             <div style={{ fontSize: '4vmin' }}>チ。</div>
@@ -38,6 +38,7 @@ const Hobby: React.FC = () => {
                         </div>
                         <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
                             <iframe style={{ borderRadius: '12px', maxWidth: "90%", height: '30vh' }} src="https://open.spotify.com/embed/track/6UHwmfYfRm445uyyM9HO3o?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                            <iframe style={{ borderRadius: '12px', maxWidth: "90%", height: '30vh' }} src="https://open.spotify.com/embed/artist/5C8KyBfvAz9PSaOd30eIow?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         </div>
                     </div>
                 </div>
